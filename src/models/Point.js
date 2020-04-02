f8a07133d3df6e210ff2232f9d894b03957cc899
Gabriel Davi <gdavi.gd@gmail.com>
2020-04-02 19:57:37 -0300

    #US12 fix errors on user controll
    Co-authored-by: lucassoaresouza <lucas.soaresouza@gmail.com>

--- src/models/Point.js
+++ src/models/Point.js
@@ -1,14 +1,14 @@
-const mongoose = require['mongoose'];
-const Point =  new mongoose.Schema({
-    type:{
-        type: String,
-        enum: ['Point'],
-        required: true
-    },
-    coordinates: {
-        type: [Number],
-        required: true
-    }
+const mongoose = require("mongoose");
+const Point = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["Point"],
+    required: true
+  },
+  coordinates: {
+    type: [Number],
+    required: true
+  }
 });
 
-module.exports = Point
+module.exports = Point;
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,14 +1,12 @@
 const UserController = require("../controllers/UserController");
 const userController = new UserController();
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-
-routes.post('/user', async (req, res, next) => {
-    userController.createUser(req, res, next)
+routes.post("/user", async (req, res, next) => {
+  userController.createUser(req, res, next);
 });
-routes.get('/user/:id', async (req, res, next) => {
-    userController.getUserById(req, res, next)
-})
-
-module.exports = routes
+routes.get("/user/:id", async (req, res, next) => {
+  userController.getUserById(req, res, next);
+});
+module.exports = routes;
