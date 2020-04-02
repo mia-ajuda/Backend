399e5089c68a8d238dc071376546afd1f3a005f1
Lucas Souza <lucas.soaresouza@gmail.com>
2020-04-02 19:23:43 -0300

    US12 Adding a point schema to users location
    Co-authored-by: gabrieldvpereira <gdavi.gd@gmail.com>

---
+++ src/models/Point.js
@@ -0,0 +1,14 @@
+const mongoose = require['mongoose'];
+const Point =  new mongoose.Schema({
+    type:{
+        type: String,
+        enum: ['Point'],
+        required: true
+    },
+    coordinates: {
+        type: [Number],
+        required: true
+    }
+});
+
+module.exports = Point
