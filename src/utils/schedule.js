7ec21ed89eadfb5714423b120cb22d69dfe8a9eb
Eduardo <eduardolima.df@gmail.com>
2020-04-06 15:48:25 -0300

    Finishing merge with develop and correcting notification function

--- src/utils/schedule.js
+++ src/utils/schedule.js
@@ -1,16 +1,19 @@
 const HelpService = require('../services/HelpService')
 const UserService = require('../services/UserService')
-const notification = require('./Notification')
+const notify = require('./Notification')
 const nodeSchedule = require('node-schedule')
 
 function dailySchedule() {
-    nodeSchedule.scheduleJob('* * 08,18 * * *', async () => {
+    nodeSchedule.scheduleJob('5 * * * * *', async () => {
         const helpService = new HelpService();
         const userService = new UserService();
         const helpsToDelete = await helpService.getListToDelete()
+        console.log(helpsToDelete)
         let messages = []
         for (let help of helpsToDelete) {
             let user = userService.getUser(help.ownerId)
+            console.log(help)
+            console.log(user)
             let message = {
                 to: user.deviceId,
                 sound: 'default',
@@ -19,7 +22,12 @@ function dailySchedule() {
             }
             messages.push(message)
         }
-        notification.notify(messages)
+        console.log(messages[0])
+        try {
+            notify(messages)
+        } catch (err) {
+            console.log(err)
+        }
         return await Promise.all(helpsToDelete.map(async (help) => {
             try {
                 return await helpService.delete(help)
@@ -29,5 +37,7 @@ function dailySchedule() {
         }))
     })
 }
+
+// * * 08,18 * * *
 
 module.exports = dailySchedule
