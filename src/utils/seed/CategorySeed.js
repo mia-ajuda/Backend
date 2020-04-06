7d022d6102f54033d426bf68c5db5d0bbd7c5a27
Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>
2020-04-06 15:12:56 -0300

    #16 - Describe the categories

--- src/utils/seed/CategorySeed.js
+++ src/utils/seed/CategorySeed.js
@@ -3,35 +3,35 @@ const Category = require('../../models/Category');
 const mapCategories = [
     {
         name: 'Higiene Pessoal',
-        description: 'Higiene Pessoal',
+        description: 'São considerados produtos de higiene pessoal, papel higienico, pasta dental, absorvente feminino, fraldas, entre outros.',
     },
     {
         name: 'Apoio Social',
-        description: 'Apoio Social',
+        description: 'Pedidos de apoio social são coisas imateriais, você pode pedir por alguém para conversar, por exemplo.',
     },
     {
         name: 'Itens de Proteção',
-        description: 'Itens de Proteção',
+        description: 'Pedidos de itens de proteção são pedidos de itens como mascaras, alcool gel, luvas, entre outros.',
     },
     {
         name: 'Apoio Psicológico',
-        description: 'Apoio Psicológico',
+        description: 'Pedido de apoio psicológico são direcionados à psicológos, eles podem conversar com você para te ajudar com alguma dificuldade que esteja passando',
     },
     {
         name: 'Pequenos Serviços',
-        description: 'Pequenos Serviços',
+        description: 'Pedidos de pequenos serviços são para caso você necessite de alguém para fazer um pequeno conserto, trocar uma lâmpada, entre outras coisas.',
     },
     {
         name: 'Suprimentos Básicos',
-        description: 'Suprimentos Básicos',
+        description: 'Pedidos de suprimentos básicos são para quando você precisa que alguém lhe traga algum item, como arroz, feijão.',
     },
     {
         name: 'Transporte de Emergência',
-        description: 'Transporte de Emergência',
+        description: 'Pedidos por transporte de emergência são para caso você necessite de alguém para levar você ou algum familiar ao hospital.',
     },
     {
         name: 'Apoio Físico',
-        description: 'Apoio Físico',
+        description: 'Pedidos de apoio físico são para caso precise de alguém para auxiliar em exercícios físicos, alongamentos, entre outros.',
     },
 ];
 
