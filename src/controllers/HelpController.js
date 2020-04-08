2a449b8001f530428e035dc0547da5e6dadf6d49
Gabriel Davi <gdavi.gd@gmail.com>
2020-04-08 16:08:47 -0300

    Adding more than one filter by category

--- src/controllers/HelpController.js
+++ src/controllers/HelpController.js
@@ -1,67 +1,75 @@
-const HelpService = require('../services/HelpService');
+const HelpService = require("../services/HelpService");
 
 class HelpController {
-    constructor() {
-        this.HelpService = new HelpService();
+  constructor() {
+    this.HelpService = new HelpService();
+  }
+
+  async createHelp(req, res, next) {
+    const data = {
+      ...req.body,
+    };
+
+    try {
+      const result = await this.HelpService.createHelp(data);
+      res.status(201).json(result);
+      next();
+    } catch (err) {
+      res.status(400).send({ error: err });
+      next();
     }
+  }
 
-    async createHelp(req, res, next) {
-        const data = {
-            ...req.body,
-        };
-
-        try {
-            const result = await this.HelpService.createHelp(data);
-            res.status(201).json(result);
-            next();
-        } catch (err) {
-            res.status(400).send({ error: err });
-            next();
-        }
+  async getHelpById(req, res, next) {
+    const { id } = req.params;
+    try {
+      const result = await this.HelpService.getHelpByid(id);
+      res.status(200).json(result);
+      next();
+    } catch (err) {
+      res.status(400).json({ error: err });
+      next();
     }
+  }
 
-    async getHelpById(req, res, next) {
-        const { id } = req.params;
-        try {
-            const result = await this.HelpService.getHelpByid(id);
-            res.status(200).json(result);
-            next();
-        } catch (err) {
-            res.status(400).json({ error: err });
-            next();
-        }
+  async getHelpList(req, res, next) {
+    const except = !!req.query["id.except"];
+    const helper = !!req.query["id.helper"];
+    const temp = except ? "except" : helper ? "helper" : null;
+    const id = temp ? req.query[`id.${temp}`] : req.query.id;
+    const status = req.query.status || null;
+    const category = req.query.categoryId
+      ? req.query.categoryId.split(",")
+      : null;
+    try {
+      let result;
+      result = await this.HelpService.getHelpList(
+        id,
+        status,
+        category,
+        except,
+        helper
+      );
+      res.status(200).json(result);
+      next();
+    } catch (err) {
+      res.status(400).json({ error: err });
+      next();
     }
+  }
 
-    async getHelpList(req, res, next) {
-        const except = req.query["id.except"] ? true : false;
-        const helper = req.query["id.helper"] ? true : false;
-        const temp = except ? 'except' : helper ? 'helper' : null;
-        const id = temp ? req.query[`id.${temp}`] : req.query.id;
-        const status = req.query.status || null;
-        const category = req.query.categoryId || null;
-        try {
-            let result;
-            result = await this.HelpService.getHelpList(id, status, category, except, helper);
-            res.status(200).json(result);
-            next();
-        } catch (err) {
-            res.status(400).json({ error: err });
-            next();
-        }
+  async deleteHelpLogic(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      const result = await this.HelpService.deleteHelpLogically(id);
+      res.status(200).json(result);
+      return next();
+    } catch (err) {
+      res.status(400).json(err);
+      return next();
     }
-
-    async deleteHelpLogic(req, res, next) {
-        const id = req.params.id;
-
-        try {
-            const result = await this.HelpService.deleteHelpLogically(id);
-            res.status(200).json(result);
-            return next();
-        } catch (err) {
-            res.status(400).json(err);
-            return next();
-        }
-    }
+  }
 }
 
 module.exports = HelpController;
