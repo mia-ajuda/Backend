1b1b00b61f36f3993895291f3521e1f82521dae5
Eduardo <eduardolima.df@gmail.com>
2020-04-13 14:18:37 -0300

    Correcting rebase errors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dailySchedule = require('./src/utils/schedule')
 const app = express()
 
 const databaseConnect = require('./src/config/database')
-const dailySchedule = require('./src/utils/schedule')
+
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -17,6 +17,6 @@ app.use(bodyParser.json())
 databaseConnect()
 dailySchedule()
 setRoutes(app)
-dailySchedule()
+
 
 app.listen(8000)
--- src/routes/HelpRoutes.js
+++ src/routes/HelpRoutes.js
@@ -9,7 +9,7 @@ const routes = express.Router();
 routes.post('/help', async (req, res, next) => {
     helpController.createHelp(req, res, next);
 });
-routes.get('/help/getOne/:id', async (req, res, next) => {
+routes.get('/help/:id', async (req, res, next) => {
     helpController.getHelpById(req, res, next);
 });
 routes.get('/help', async (req, res, next) => {
