659ec35b60b6eb49a453ec3fd1fc1519322e0cdb
Eduardo <eduardolima.df@gmail.com>
2020-04-14 13:02:44 -0300

    Hotfix US24 adding new status

--- src/utils/enums/helpStatusEnum.js
+++ src/utils/enums/helpStatusEnum.js
@@ -1,6 +1,8 @@
 const helpStatusEnum = {
     WAITING: "waiting",
     ON_GOING: "on_going",
-    FINISHED: "finished"
+    FINISHED: "finished",
+    OWNER_FINISHED: "owner_finished",
+    HELPER_FINISHED: "helper_finished"
 }
 module.exports = helpStatusEnum
