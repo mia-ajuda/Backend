2ab0d3f511526370ffb771745e6e714fa777793a
Welison Almeida <welison.almeida.923@gmail.com>
2020-04-24 12:41:59 -0300

    Add Notification model to store user's notification history

---
+++ src/models/Notification.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const notificationTypes = {
+    ajudaRecebida: 'Seu pedido recebeu uma oferta de ajuda!',
+    ajudaAceita: 'Sua oferta de ajuda foi aceita!',
+    ajudaFinalizada: 'Seu pedido de ajuda foi finalizado!',
+    ajudaExpirada: 'Seu pedido de ajuda expirou!',
+    outros: 'Demais tipos de notificação!',
+};
+
+const notificationTypesEnum = {
+    ajudaRecebida: 'ajudaRecebida',
+    ajudaAceita: 'ajudaAceita',
+    ajudaFinalizada: 'ajudaFinalizada',
+    ajudaExpirada: 'ajudaExpirada',
+    outros: 'outros',
+};
+
+const Notification = new mongoose.Schema({
+    helpId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Help',
+        required: false,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    registerDate: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    notificationType: {
+        type: String,
+        enum: [...Object.keys(notificationTypesEnum)],
+    },
+}, { 
+    collection: 'notification',
+});
+
+module.exports = {
+    Notification,
+    notificationTypes,
+    notificationTypesEnum
+}
