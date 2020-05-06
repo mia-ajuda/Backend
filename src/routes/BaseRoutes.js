931081ecb599b205f4e262eaeff29d3db5013801
Welison Almeida <welison.almeida.923@gmail.com>
2020-05-06 18:59:44 -0300

    Set up swagger API documentation

--- src/routes/BaseRoutes.js
+++ src/routes/BaseRoutes.js
@@ -3,6 +3,11 @@ const helpRoutes = require('./HelpRoutes');
 const categoryRoutes = require('./CategoryRoutes');
 const notificationRoutes = require('./NotificationRoutes');
 
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load('docs/swagger.yaml');
+
 module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     app.use('/api', [userRoutes, helpRoutes, categoryRoutes, notificationRoutes]);
 };
