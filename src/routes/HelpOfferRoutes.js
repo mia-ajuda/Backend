babdbf222eb6fd522568e1d2087d14676b982ec7
Gabriel Davi <gdavi.gd@gmail.com>
2020-08-03 19:29:13 -0300

    Change help request route name

--- src/routes/HelpOfferRoutes.js
+++ src/routes/HelpOfferRoutes.js
@@ -5,10 +5,10 @@ const isAuthenticated = require('../validation/middlewares/authFirebase');
 const helpOfferController = new HelpOfferController();
 const routes = express.Router();
 
-routes.post('/helpOffer/create', isAuthenticated, (req, res, next) => {
+routes.post('/helpOffer', isAuthenticated, (req, res, next) => {
   helpOfferController.createHelpOffer(req, res, next);
 });
-routes.get('/helpOffer/list', isAuthenticated ,(req, res, next) => {
+routes.get('/helpOffer/list', isAuthenticated, (req, res, next) => {
   helpOfferController.listHelpsOffers(req, res, next);
 });
 
@@ -16,12 +16,20 @@ routes.get('/helpOffer/list/:ownerId', isAuthenticated, (req, res, next) => {
   helpOfferController.listHelpsOffersByOwnerId(req, res, next);
 });
 
-routes.get('/helpOffer/list/:helpedUserId', isAuthenticated, (req, res, next) => {
-  helpOfferController.listHelpOffersByHelpedUserId(req, res, next);
-});
+routes.get(
+  '/helpOffer/list/:helpedUserId',
+  isAuthenticated,
+  (req, res, next) => {
+    helpOfferController.listHelpOffersByHelpedUserId(req, res, next);
+  },
+);
 
-routes.put('/helpOffer/possibleHelpedUsers/:helpedId/:helpOfferId', isAuthenticated, (req, res, next) => {
-  helpOfferController.addPossibleHelpedUsers(req, res, next);
-});
+routes.put(
+  '/helpOffer/possibleHelpedUsers/:helpedId/:helpOfferId',
+  isAuthenticated,
+  (req, res, next) => {
+    helpOfferController.addPossibleHelpedUsers(req, res, next);
+  },
+);
 
 module.exports = routes;
