9b0129d9150c8432b8d625e7d79dfa334878b8b5
Danillo <danillosouza1704@gmail.com>
2020-08-11 14:21:27 -0300

    Fixed routes with same name

--- src/routes/CampaignRoutes.js
+++ src/routes/CampaignRoutes.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const CampaignController = require('../controllers/CampaignController');
-const isAuthenticated = require('../validation/middlewares/authFirebase');
+const express = require("express");
+const CampaignController = require("../controllers/CampaignController");
+const isAuthenticated = require("../validation/middlewares/authFirebase");
 
 const campaignController = new CampaignController();
 const routes = express.Router();
 
-routes.post('/campaign', isAuthenticated, (req, res, next) => {
+routes.post("/campaign", isAuthenticated, (req, res, next) => {
   campaignController.createCampaign(req, res, next);
 });
-routes.get('/campaign', isAuthenticated, (req, res, next) => {
+routes.get("/campaign", isAuthenticated, (req, res, next) => {
   campaignController.listCampaignNear(req, res, next);
 });
-routes.get('/campaign', isAuthenticated, (req, res, next) => {
+routes.get("/campaign/list", isAuthenticated, (req, res, next) => {
   campaignController.listCampaignByOwnerId(req, res, next);
-})
-routes.delete('/campaign', isAuthenticated, async (req, res, next) => {
-    campaignController.deleteCampaign(req, res, next);
 });
-  
+routes.delete("/campaign", isAuthenticated, async (req, res, next) => {
+  campaignController.deleteCampaign(req, res, next);
+});
+
 module.exports = routes;
