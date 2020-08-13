f4d1bce9fc788974e2e967895016be4e4e37c412
Danillo <danillosouza1704@gmail.com>
2020-08-13 15:49:40 -0300

    Now help offers of an entity are listed

--- src/services/HelpService.js
+++ src/services/HelpService.js
@@ -315,8 +315,8 @@ class HelpService {
       isUser = true;
     } catch {
       helper = await this.EntityService.getEntity({ id: idHelper });
+      isUser = true;
     }
-
     if (isUser) {
       const userPosition = help.possibleHelpers.indexOf(idHelper);
 
