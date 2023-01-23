c5b51aed40deb57c50245b913b1592955fae6297
sudjoao <isudjoao@gmail.com>
2023-01-23 12:40:09 -0300

    chore: change title and description in seeds to pt-br

---
+++ src/utils/seed/mockedInfos/mockedOfferInfo.js
@@ -0,0 +1,20 @@
+const mockedOfferInfo = {
+    "titles": [
+        "Doando comida",
+        "Cestas básicas",
+        "Cuidador de idosos",
+        "Consultas grátis",
+        "Agasalhos sobrando",
+        "Carona",
+    ],
+    "descriptions":[
+        "Tenho arroz e feijão para doar",
+        "Tenho 7 cestas básicas sobrando aqui",
+        "Estou ofertando meu serviço de cuidador de idosos gratuitamente",
+        "Precisando de ajuda psicológica? Posso te ajudar.",
+        "Estou com alguns agasalhos prontos para doação.",
+        "Posso ajudar na locomoção de idosos",
+    ],
+}
+
+module.exports = mockedOfferInfo;
