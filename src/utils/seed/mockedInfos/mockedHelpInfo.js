08ca7dca828b3ea59e53cdbd9e7df4cd9b1a4292
sudjoao <isudjoao@gmail.com>
2023-01-23 12:44:18 -0300

    fix: correct lint

--- src/utils/seed/mockedInfos/mockedHelpInfo.js
+++ src/utils/seed/mockedInfos/mockedHelpInfo.js
@@ -1,20 +1,20 @@
 const mockedHelpInfo = {
-    "titles": [
-        "Preciso de Ajuda",
-        "Alguém pode me Ajudar?",
-        "Baile Beneficente",
-        "Preciso de comida",
-        "Apoio Social",
-        "Preciso de Agasalhos",
-    ],
-    "descriptions":[
-        "Preciso de novas mascáras",
-        "A água aqui de casa acabou",
-        "Precisando de voluntário para organizar baile beneficente",
-        "Alguém pode me ajudar dando arroz?",
-        "Estou me sentido sozinho durante esse isolamento.",
-        "Estou passando muito frio.",
-    ],
-}
+  titles: [
+    'Preciso de Ajuda',
+    'Alguém pode me Ajudar?',
+    'Baile Beneficente',
+    'Preciso de comida',
+    'Apoio Social',
+    'Preciso de Agasalhos',
+  ],
+  descriptions: [
+    'Preciso de novas mascáras',
+    'A água aqui de casa acabou',
+    'Precisando de voluntário para organizar baile beneficente',
+    'Alguém pode me ajudar dando arroz?',
+    'Estou me sentido sozinho durante esse isolamento.',
+    'Estou passando muito frio.',
+  ],
+};
 
 module.exports = mockedHelpInfo;
