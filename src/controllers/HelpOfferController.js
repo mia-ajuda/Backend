38ad36e5e976dbe5c4a1c851d4032f7c85f0c1e2
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-06-23 02:48:32 -0300

    fix: fix missing category array

--- src/controllers/HelpOfferController.js
+++ src/controllers/HelpOfferController.js
@@ -36,8 +36,15 @@ class OfferedHelpController {
     const getOtherUsers = req.query.getOtherUsers === 'true';
     const { isUserEntity } = global;
     const coords = req.query.coords.split(',').map((coord) => Number(coord));
+    const categoryArray = req.query.categoryId ? req.query.categoryId.split(',') : null;
     try {
-      const helpOffers = await this.HelpOfferService.listHelpsOffers(coords, userId, isUserEntity, null, getOtherUsers);
+      const helpOffers = await this.HelpOfferService.listHelpsOffers(
+        coords,
+        userId,
+        isUserEntity,
+        categoryArray,
+        getOtherUsers,
+      );
       return res.json(helpOffers);
     } catch (error) {
       return res.status(400).json({ error: error.message });
